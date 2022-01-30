6dc365fae029d6ed8480b8a628acd00a62a576ff
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-01-30 07:51:08 +0530

    Update prob7.cs

--- Basic Programs/Basic Programs/prob7.cs
+++ Basic Programs/Basic Programs/prob7.cs
@@ -8,19 +8,15 @@ namespace BasicPrograms
 {
     internal class BasicPrograms
     {
-        public void SwapingOfNumbers()
+        public void Quotientremainder(int dividend, int divisor)
         {
-            int a, b;
-            Console.WriteLine("Enter the value of a");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter the value of b");
-            b = Convert.ToInt32(Console.ReadLine());
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
 
-            Console.WriteLine("Before swap a= " + a + " b= " + b);
-            a = a * b;
-            b = a / b;
-            a = a / b;
-            Console.Write("After swap a= " + a + " b= " + b);
+            Console.WriteLine("Dividend:" + dividend + " Divisor: " + divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+     
         }
     }
 }
