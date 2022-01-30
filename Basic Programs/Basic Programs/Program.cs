cacadf3fe107d7520e176a7d6e12fef1b3c3ef61
rahul <rahulravi2502@gmail.com>
2022-01-30 07:27:27 +0530

    Adding  Powertwo

--- Basic Programs/Basic Programs/Program.cs
+++ Basic Programs/Basic Programs/Program.cs
@@ -12,7 +12,7 @@ namespace BasicPrograms
         {
             Console.WriteLine("Welcome to basic program");
             BasicPrograms prob= new BasicPrograms();
-            prob.Check_leap_year();
+            prob.Powertwo();
             Console.ReadLine();
         }
     }
