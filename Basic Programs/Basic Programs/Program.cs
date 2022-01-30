f42338a3d4e013ebb66dbdedff6535483dcad812
rahul <rahulravi2502@gmail.com>
2022-01-30 07:33:39 +0530

    Adding prime

--- Basic Programs/Basic Programs/Program.cs
+++ Basic Programs/Basic Programs/Program.cs
@@ -12,7 +12,7 @@ namespace BasicPrograms
         {
             Console.WriteLine("Welcome to basic program");
             BasicPrograms prob= new BasicPrograms();
-            prob.Factorization();
+            prob.Factorization(36);
             Console.ReadLine();
         }
     }
