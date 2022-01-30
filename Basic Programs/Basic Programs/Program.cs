c61de9569c8f0b49e64f8dee20bdbd81097c128a
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-01-30 08:02:43 +0530

    Update Program.cs

--- Basic Programs/Basic Programs/Program.cs
+++ Basic Programs/Basic Programs/Program.cs
@@ -12,7 +12,7 @@ namespace BasicPrograms
         {
             Console.WriteLine("Welcome to basic program");
             BasicPrograms prob = new BasicPrograms();
-            prob.largest_num();
+            prob.vowel_consonant();
             Console.ReadLine();
         }
     }
