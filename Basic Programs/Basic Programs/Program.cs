2db8aead0d077ded710ec3106df380cea474d4ac
rahul <rahulravi2502@gmail.com>
2022-01-30 07:30:17 +0530

    Adding Harmonic

--- Basic Programs/Basic Programs/Program.cs
+++ Basic Programs/Basic Programs/Program.cs
@@ -12,7 +12,7 @@ namespace BasicPrograms
         {
             Console.WriteLine("Welcome to basic program");
             BasicPrograms prob= new BasicPrograms();
-            prob.Powertwo();
+            prob.Harmonic_number();
             Console.ReadLine();
         }
     }
